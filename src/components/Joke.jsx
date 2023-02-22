d439f635817667f5ffa819182f8a3daf1eaac055
t-flim <fulim1988@gmail.com>
2023-02-22 10:39:07 +0800

    Add Joke component

---
+++ src/components/Joke.jsx
@@ -0,0 +1,22 @@
+
+
+function Joke() {
+
+    return (
+        <div className="joke">
+            <div className="joke-content">
+                <h4 className="question">
+                What did spider do on the computer?
+                </h4>
+                <p className="punchline">
+                Made a website.
+                </p>
+            </div>
+            <div className="buttons">
+                <button className="fetch">New Joke</button>
+            </div>
+        </div>
+    )
+}
+
+export default Joke
