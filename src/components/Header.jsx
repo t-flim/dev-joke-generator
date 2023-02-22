985e2ffd4558aa95a961019bad9991cfdfd05459
t-flim <fulim1988@gmail.com>
2023-02-22 15:59:02 +0800

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../App.css"
+
+function Header() {
+
+    return (
+        <header id="header">
+            <h1 id="header-title">Dev Joke</h1>
+        </header>
+    )
+}
+
+export default Header
