0a0b4617cd7f8e763fd1219e826875b6544cf214
t-flim <fulim1988@gmail.com>
2023-02-22 15:42:22 +0800

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "../App.css"
+
+function Footer() {
+
+    return (
+        <footer id="footer">
+            <p className="footer-content">
+                &copy; 2023 | thefulim.my
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
