5d9862c732a0c0c110f543fe975fd9e978a9b96a
t-flim <fulim1988@gmail.com>
2023-02-22 13:02:47 +0800

    Add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+
+
+function Button() {
+
+    return (
+        <div className="buttons">
+            <button className="fetch-joke">Next Joke</button>
+        </div>
+    )
+}
+
+export default Button
