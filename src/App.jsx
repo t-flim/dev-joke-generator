9a919a9b6ae799d378278a817d2ee4ec8c05911a
t-flim <fulim1988@gmail.com>
2023-02-22 11:11:26 +0800

    Import Loader to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import useFetch from "./custom-hook/useFetch"
 
 // components
 import Joke from "./components/Joke"
+import Loader from "./components/Loader"
 
 function App() {
   const url = "https://backend-omega-seven.vercel.app/api/getjoke"
