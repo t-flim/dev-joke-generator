9ba1cef687859c1e21ce7ab841af25540c14d0fe
t-flim <fulim1988@gmail.com>
2023-02-22 11:02:03 +0800

    Import useFetch to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css"
 
+// hooks
+import useFetch from "./custom-hook/useFetch"
+
 // components
 import Joke from "./components/Joke"
 
 function App() {
+  const url = "https://backend-omega-seven.vercel.app/api/getjoke"
+  const { data:joke, isLoading, error, fetchData:fetchJoke } = useFetch(url)
 
   return (
     <div className="app">
